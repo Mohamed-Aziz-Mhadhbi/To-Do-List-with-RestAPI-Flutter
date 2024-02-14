e9de62dea509eb2e973ff7d7a1bb0f6389ce41bd
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2024-02-14 14:04:03 +0100

    udpate

--- lib/screens/todo_list.dart
+++ lib/screens/todo_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todolist/screens/add_page.dart';
+import 'package:http/http.dart' as http;
 
 class TodoListPage extends StatefulWidget {
   const TodoListPage({super.key});
@@ -26,5 +27,13 @@ class _TodoListPageState extends State<TodoListPage> {
       builder: (context) => const AddTodoPage(),
     );
     Navigator.push(context, route);
+  }
+
+  Future<void> fetchTodo() async {
+    const url = "https://api.nstack.in/v1/todos?page=1&limit=10";
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    if (response.statusCode == 200) {
+    } else {}
   }
 }
